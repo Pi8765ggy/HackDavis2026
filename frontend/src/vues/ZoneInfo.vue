20112c60c861cf6b166a32cdc0c2003eaeb499ab
Itsuki Hirano <ihirano@ucdavis.edu>
2026-05-09 23:30:31 -0700

    zone view design

--- frontend/src/vues/ZoneInfo.vue
+++ frontend/src/vues/ZoneInfo.vue
@@ -1,64 +1,112 @@
+<script>
+    import {ref} from 'vue'
+
+    const showResult =  ref(false)
+
+    function switchView() {
+        showResult.value = false
+    }
+
+    function onSubmit() {
+        showResult.value = true
+    }
+</script>
+
 <template>
     <header>
         <img src="../images/logo-full.svg" alt="gardinspiration logo" id="logo">
         <img src="../images/user-icon.svg" alt="user icon" id="user">
     </header>
 
-    <h1>Find the perfect plants for your garden</h1>
-    
+    <!-- toggle the class "invis" switch display -->
+    <!-- <div id="intialPage" class="invis"> -->
+    <div id="intialPage">
+        <h1>Find the perfect plants for your garden</h1>
+        
+        <form @submit.prevent="onSubmit">
+            <h2>Enter your ZIP Code</h2>
 
-    <form>
-        <h2>Enter your ZIP Code</h2>
+            <input type="text" id="zip" name="zip">
 
-        <input type="text" id="zip" name="zip">
+            <input type="submit" value="START" id="submit">
+        </form>
 
-        <input type="submit" value="START" id="submit">
-    </form>
+        <div id="img-container">
+                <div class="gallery" id="row1">
+                <img src="../images/tree1.jpg" alt="tree 1">
+                <img src="../images/tree2.jpg" alt="tree 2">
+                <img src="../images/flower1.jpg" alt="flower 1">
+                <img src="../images/bush1.jpg" alt="bush 1">
+                <img src="../images/bush6.jpg" alt="bush 2">
+                <img src="../images/tree1.jpg" alt="tree 1">
+                <img src="../images/tree2.jpg" alt="tree 2">
+                <img src="../images/flower1.jpg" alt="flower 1">
+                <img src="../images/bush1.jpg" alt="bush 1">
+                <img src="../images/bush6.jpg" alt="bush 2">
+                <img src="../images/tree1.jpg" alt="tree 1">
+                <img src="../images/tree2.jpg" alt="tree 2">
+                <img src="../images/flower1.jpg" alt="flower 1">
+                <img src="../images/bush1.jpg" alt="bush 1">
+                <img src="../images/bush6.jpg" alt="bush 2">
+            </div>
 
-    <div id="img-container">
-            <div class="gallery" id="row1">
-            <img src="../images/tree1.jpg" alt="tree 1">
-            <img src="../images/tree2.jpg" alt="tree 2">
-            <img src="../images/flower1.jpg" alt="flower 1">
-            <img src="../images/bush1.jpg" alt="bush 1">
-            <img src="../images/bush6.jpg" alt="bush 2">
-            <img src="../images/tree1.jpg" alt="tree 1">
-            <img src="../images/tree2.jpg" alt="tree 2">
-            <img src="../images/flower1.jpg" alt="flower 1">
-            <img src="../images/bush1.jpg" alt="bush 1">
-            <img src="../images/bush6.jpg" alt="bush 2">
-            <img src="../images/tree1.jpg" alt="tree 1">
-            <img src="../images/tree2.jpg" alt="tree 2">
-            <img src="../images/flower1.jpg" alt="flower 1">
-            <img src="../images/bush1.jpg" alt="bush 1">
-            <img src="../images/bush6.jpg" alt="bush 2">
+            <div class="gallery" id="row2">
+                <img src="../images/tree3.jpg" alt="tree 3">
+                <img src="../images/tree4.jpg" alt="tree 4">
+                <img src="../images/bush3.jpg" alt="bush 3">
+                <img src="../images/tree5.jpg" alt="tree 5">
+                <img src="../images/bush4.jpg" alt="bush 4">
+                <img src="../images/tree3.jpg" alt="tree 3">
+                <img src="../images/tree4.jpg" alt="tree 4">
+                <img src="../images/bush3.jpg" alt="bush 3">
+                <img src="../images/tree5.jpg" alt="tree 5">
+                <img src="../images/bush4.jpg" alt="bush 4">
+                <img src="../images/tree3.jpg" alt="tree 3">
+                <img src="../images/tree4.jpg" alt="tree 4">
+                <img src="../images/bush3.jpg" alt="bush 3">
+                <img src="../images/tree5.jpg" alt="tree 5">
+                <img src="../images/bush4.jpg" alt="bush 4">
+            </div>
+
         </div>
-
-        <div class="gallery" id="row2">
-            <img src="../images/tree3.jpg" alt="tree 3">
-            <img src="../images/tree4.jpg" alt="tree 4">
-            <img src="../images/bush3.jpg" alt="bush 3">
-            <img src="../images/tree5.jpg" alt="tree 5">
-            <img src="../images/bush4.jpg" alt="bush 4">
-            <img src="../images/tree3.jpg" alt="tree 3">
-            <img src="../images/tree4.jpg" alt="tree 4">
-            <img src="../images/bush3.jpg" alt="bush 3">
-            <img src="../images/tree5.jpg" alt="tree 5">
-            <img src="../images/bush4.jpg" alt="bush 4">
-            <img src="../images/tree3.jpg" alt="tree 3">
-            <img src="../images/tree4.jpg" alt="tree 4">
-            <img src="../images/bush3.jpg" alt="bush 3">
-            <img src="../images/tree5.jpg" alt="tree 5">
-            <img src="../images/bush4.jpg" alt="bush 4">
-        </div>
-
     </div>
 
-    
-    
+    <!-- toggle class "hidden" to switch display-->
+    <div id="resultView" class="hidden"> 
+        <section id="zone">
+            <p>Your Zone is:</p>
+            <h2>Davis Zone</h2>
+
+            <button id="reset">Enter another location</button>
+
+            <!-- if logged in, remove the words "Log-in to" -->
+            <button id="garden">Log-in to manage your garden</button>
+        </section>
+
+        <section id="showPlants">
+            <h3>Here are some plants we recommend for your location</h3>
+            <article>
+                <img src="../images/flower1.jpg" alt="flower">
+                <div>
+                    <h2>Flower 1</h2>
+                    <p>Despcription for the plant goes here</p>
+                </div>   
+            </article>
+
+            <article>
+                <img src="../images/tree1.jpg" alt="tree">
+                <div>
+                    <h2>tree 1</h2>
+                    <p>Despcription for the plant goes here</p>
+                </div>   
+            </article>
+        </section>
+        <!-- <button id="reset" @click="switchView">Click Me</button> -->
+    </div>
 </template>
 
 <style>
+    /* shared */
     h1 {
         font-weight: 500;
         text-align: center;
@@ -68,6 +116,84 @@
         color: #FFFAC7;
     }
 
+    .invis {
+        display: none;
+    }
+
+    /* zone display page */
+    #resultView {
+        box-sizing: border-box;
+        position: absolute;
+        z-index: -1;
+        top: 120px;
+        transform: translateY(0);
+        transition: transform 1s ease-in-out;
+        background-color: #58554A;
+        width: 100vw;
+        padding:0 15px;
+        display: flex;
+        gap: 20px;
+        align-items: stretch;
+    }
+
+    #resultView.hidden {
+        display: none;
+        transform: translateY(-100%);
+    }
+
+    #zone {
+        flex: 1;
+        background-color: #8BBF56;
+        text-align: center;
+        color: #403520;
+        font-size: 24px;
+    }
+
+    #zone h2 {
+        font-size: 36px;
+        color: #403520;
+        margin-top: 30px;
+    }
+
+    #zone button {
+        display: block;
+        background-color: #FFFAC7;
+        color: #403520;
+        border: none;
+        border-radius: 20px;
+        cursor: pointer;
+        font-size: 20px;
+        padding: 15px;
+        margin: 20px auto;
+    }
+
+    #showPlants {
+        flex: 2;
+        padding: 0 50px;
+        box-sizing: border-box;
+    }
+
+    #showPlants h3 {
+        font-weight: 400;
+    }
+
+    article {
+        display: flex;
+        gap: 30px;
+        border: solid 4px #9FE459;
+        border-radius: 20px;
+        margin-bottom: 20px;
+    }
+
+    article img {
+        flex: 1;
+        max-width: 30%;
+        aspect-ratio: 1;
+        box-sizing: border-box;
+        padding: 15px;
+    }
+
+    /* zip code input page */
     form {
         width: 95%;
         border-radius: 20px;
